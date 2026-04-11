4a17273fcc63d6016b2b498b6ffbfed307524b99
NikosKatrakoulis <nikoskatrakoulis@gmail.com>
2026-04-11 10:34:08 +0300

    Factoring the first choice

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main
         String [] taskTitles = new String[10];
         boolean [] taskDone = new boolean[10];
         int taskCount = 0;
+        boolean isRunning = true;
 
         while (true) {
 
@@ -21,21 +22,34 @@ public class Main
             System.out.println("5. Quit");
             System.out.print("Choose one: ");
             int menuChoice = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println();
 
-            if (menuChoice == 1) {
-                return;
-            } else if (menuChoice == 2) {
-                return;
-            } else if (menuChoice == 3) {
-                return;
-            } else if (menuChoice == 4) {
-                return;
-            } else if (menuChoice == 5) {
-                return;
-            } else {
-                return;
+            while (isRunning) {
+
+                if (menuChoice == 1) {
+                    System.out.println("Enter the task you want to add: ");
+                    String task = scanner.nextLine().trim();
+                    if (task.equals("exit")){
+                        isRunning = false;
+                        break;
+                    }
+                    taskTitles[taskCount] = task;
+                    taskDone[taskCount] = false;
+                    taskCount++;
+
+                } else if (menuChoice == 2) {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("ID: " + taskTitles[i]);
+                    }
+                } else if (menuChoice == 3) {
+                    return;
+                } else if (menuChoice == 4) {
+                    return;
+                } else {
+                    return;
+                }
             }
-
         }
     }
 }
