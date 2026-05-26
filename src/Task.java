898cd40e35427e991b18421112187c94ded6d89e
NikosKatrakoulis <nikoskatrakoulis@gmail.com>
2026-05-26 18:05:55 +0300

    Added attributes, constructor, getters, state of done and toString

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,34 @@
 public class Task {
 
+    private int id;
+    private String title;
+    private boolean done;
 
+    public Task(int id, String title) {
+        this.id = id;
+        this.title = title;
+        done = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void markAsDone() {
+        this.done = true;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID: [ %d ] | Task: [ %-10s ] | State: [ %b ]",
+                id, title, isDone());
+    }
 }
