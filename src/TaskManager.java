721b05469110758f7eb1e1fd45481577a7a6b529
NikosKatrakoulis <nikoskatrakoulis@gmail.com>
2026-05-27 19:39:14 +0300

    Added viewTasks, completeTask

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -20,6 +20,23 @@ public class TaskManager {
         nextId++;
     }
 
+    public void viewTasks() {
+        int counter = 0;
+        if (taskCount == 0) {
+            System.out.println("No tasks found.");
+            System.out.println();
+        } else {
+            for (Task task : tasks) {
+                System.out.println(task);
+                counter++;
+
+                if (counter >= taskCount) {
+                    break;
+                }
+            }
+        }
+    }
+
     private void increaseSize() {
         Task[] temp = new Task[tasks.length * 2];
 
@@ -27,5 +44,26 @@ public class TaskManager {
             temp[task] = tasks[task];
         }
         tasks = temp;
+    }
+
+    public void completeTask(int id) {
+
+        boolean found = false;
+
+        if (taskCount == 0) {
+            System.out.println("No tasks found.");
+        } else {
+            for (int task = 0; task < taskCount; task++) {
+                if (tasks[task].getId() == id) {
+                    tasks[task].markAsDone();
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                System.out.println("Invalid choice! Please enter the existing number of the task.");
+            }
+
+        }
     }
 }
